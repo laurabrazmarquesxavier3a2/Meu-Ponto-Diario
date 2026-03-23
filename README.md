54c19a48d4c88aa48992200ea43fe03f81366d59
anabiavidli <anabeatrizvidli@gmail.com>
2026-03-22 22:11:22 -0300

    arrumar pastas

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Meu-Ponto-Diario
 TCC - Técnico de Desenvolvimento de Sistemas
+
+alunos:
+ana beatriz vidal de lima
+laura braz marques
+lyvia santos
+guilherme gonçalves alves
+carlos santos
--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-//requer que haja o arquivo config.php configurado
-require_once 'config.php';
-
-$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if(!$con){
-    echo 'Falha na conexão com banco:'.$con->error;
-}
