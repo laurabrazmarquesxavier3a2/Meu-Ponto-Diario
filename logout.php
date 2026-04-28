52ad4bbf575478ecf0904dc2ee7e643c2cd25ca6
anabiavidli <anabeatrizvidli@gmail.com>
2026-04-27 21:58:51 -0300

    logout

--- logout.php
+++ logout.php
@@ -2,9 +2,6 @@
 session_start();
 session_destroy();
 
-// opcional: apagar cookies
-setcookie(session_name(), '', time() - 3600);
-
 // redireciona pro login
 header("Location: login.html");
 exit;
