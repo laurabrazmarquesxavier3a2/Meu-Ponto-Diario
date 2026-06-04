ad16911808bf2558348e5be4ddffc2b70f85c611
anabiavidli <anabeatrizvidli@gmail.com>
2026-06-03 22:30:50 -0300

    gdfgf

--- ajuda.php
+++ ajuda.php
@@ -25,12 +25,6 @@
 
         <div class="d-flex justify-content-between align-items-center mb-5 flex-wrap gap-3">
 
-            <a href="index.php" class="btn-voltar">
-                <i class="bi bi-arrow-left"></i>
-                Voltar para página inicial
-            </a>
-
-            <img src="img/logo-azul.png" alt="Meu Ponto Diário" class="logo-topo">
 
         </div>
 
