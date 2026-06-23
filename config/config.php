c326fb819a992adc600855ec6c816619793a8109
anabiavidli <anabeatrizvidli@gmail.com>
2026-06-23 17:05:45 -0300

    db

--- config/config.php
+++ config/config.php
@@ -4,3 +4,4 @@ define('DB_HOST', getenv('MYSQLHOST'));
 define('DB_USER', getenv('MYSQLUSER'));
 define('DB_PASS', getenv('MYSQLPASSWORD'));
 define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_PORT', getenv('MYSQLPORT'));
